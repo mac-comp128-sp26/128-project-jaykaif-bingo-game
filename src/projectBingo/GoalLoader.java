198df77d277af2bd1c2b7f7dba75d60474ee618e
kkassam1 <173460177+kkassam1-git@users.noreply.github.com>
2026-04-21 17:09:42 -0500

    csv to List<Tile>

---
+++ src/projectBingo/GoalLoader.java
@@ -0,0 +1,47 @@
+package projectBingo;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GoalLoader {
+
+
+    /**
+     * 
+     * @param filePath
+     * @return List of Tile objects paprsed from the
+     */
+    public static List<Tile> loadFromCSV (String filePath){
+        List<Tile> tiles = new ArrayList<>();
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))){
+            String line = reader.readLine(); // this skips header row of csv, consumes top and 
+
+            while((line = reader.readLine()) != null){
+                line = line.trim();
+                if(line.isEmpty()) continue;
+
+                String[] parts = line.split(",", 3).; // max 3 splits - parts[0] = goal, parts[1] = priority, parts[2] = category.
+                if(parts.length < 2) continue;
+
+                String goal = parts[0].trim().replace("\"", "");
+                int priority = Integer.parseInt(parts[1].trim());
+
+                tiles.add(new Tile(goal, priority));
+            }
+
+        } catch(IOException e){
+            System.err.println("Error reading CSV: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            System.err.println("Bad priority value in CSV:" + e.getMessage());
+        }
+        
+
+        return tiles;
+
+    }
+    
+}
